760b455565da794cfc03c613644776d86c27213e
bk2dcradle <ankitsultana@gmail.com>
2016-03-18 21:27:52 +0530

    done install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@ git submodule init
 for i in $(git submodule | sed -e 's/.* //'); do
     spath=$(git config -f .gitmodules --get submodule.$i.path)
     surl=$(git config -f .gitmodules --get submodule.$i.url)
-	echo $spath . $surl
+	if [ $(echo $spath | grep -c "subtitle") -ne 0 ]; then
+		git submodule update --init -- $spath --depth 1
+	else
+		git submodule update --init -- $spath
+	fi
 done
