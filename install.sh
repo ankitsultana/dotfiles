e44442e3fdbb7d10e9385c0ee9319dd194d8fb2f
bk2dcradle <ankitsultana@gmail.com>
2016-03-18 21:44:26 +0530

    try to fix

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ for i in $(git submodule | sed -e 's/.* //'); do
     spath=$(git config -f .gitmodules --get submodule.$i.path)
     surl=$(git config -f .gitmodules --get submodule.$i.url)
 	if [ $(echo $spath | grep -c "subtitle") -ne 0 ]; then
-		git submodule update --init -- $spath --depth 1
+		git submodule update --depth 1 -- $spath
 	else
 		git submodule update --init -- $spath
 	fi
