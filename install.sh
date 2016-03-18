57fe83dd3d55af83586c3a6e2dd6271f45e7949c
bk2dcradle <ankitsultana@gmail.com>
2016-03-18 21:24:51 +0530

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
-for url in $(cat .gitmodules | grep -o 'https.*$'); do
-	if [ $(echo $url | grep -c 'subtitle-downloader') -ne 0 ]; then
-		git submodule update --depth 1 $url
-	else
-		git submodule update $url
-		echo $url
-	fi
+git submodule init
+for i in $(git submodule | sed -e 's/.* //'); do
+    spath=$(git config -f .gitmodules --get submodule.$i.path)
+    surl=$(git config -f .gitmodules --get submodule.$i.url)
+	echo $spath . $surl
 done
