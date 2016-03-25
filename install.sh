efaa3cb402487a1f64603c6ccfc460fbb7521914
bk2dcradle <ankitsultana@gmail.com>
2016-03-25 19:45:02 +0530

    update

--- install.sh
+++ install.sh
@@ -12,11 +12,23 @@ for i in $(git submodule | sed -e 's/.* //'); do
 	fi
 done
 
+echo "Installing oh-my-zsh"
+echo "===================="
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+mv oh-my-zsh ~/.oh-my-zsh
+
+echo "Installing zsh theme"
+echo "===================="
+mv zsh/ankitsultana.zsh-theme ~/.oh-my-zsh/themes/
+
 echo "Installing vim plugins"
 echo "======================\n\n"
 cd vim
 echo "Installing Pathogen in vim/autoload"
-mkdir autoload
+if [ ! -d autoload ]; then
+    mkdir autoload
+    echo "Created vim/autoload"
+fi
 curl -LSso autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo "Done!\n\n"
 echo "Cloning vim plugins"
@@ -25,6 +37,19 @@ while read in; do
 	git clone $in
 	echo "\n======================================================\n"
 done < $filename
+
+if [ -d ~/.vim ]; then
+    mv ~/.vim ~/.vim.bak
+    echo "WARN: ~/.vim already exists, backing it up"
+    echo "Renamed ~/.vim to ~/.vim.bak"
+fi
+cp -r vim ~/.vim
+if [ -d ~/.vimrc ]; then
+    mv ~/.vimrc ~/.vimrc.bak
+    echo "WARN: ~/.vimrc already exists, backing it up"
+    echo "Renamed ~/.vimrc to ~/.vimrc.bak"
+fi
+mv ~/.vim/vimrc ~/.vimrc
 
 echo "Installing zsh plugins"
 echo "======================\n\n"
@@ -35,3 +60,26 @@ while read in; do
 	git clone $in
 	echo "\n======================================================\n"
 done < $filename
+
+cd ..
+
+echo "Installing utilities"
+echo "===================="
+if [ -d ~/.utilities ]; then
+    mv ~/.utilities ~/.utilities.bak
+    echo "WARN: ~/.utilities already exists, backing it up"
+    echo "Renamed ~/.utilities to ~/.utilities.bak"
+fi
+cp -r utilities ~/.utilities
+
+echo "Installing Subtitle Downloader"
+echo "=============================="
+cp -r "utilities/subtitle-downloader/Get Subtitles.workflow" "~/Library/Services"
+
+echo "Installing fzf"
+echo "=============="
+if [ -d ~/.fzf]; then
+    echo "fzf already exists!"
+else
+    cp -r iterm/fzf ~/.fzf
+fi
