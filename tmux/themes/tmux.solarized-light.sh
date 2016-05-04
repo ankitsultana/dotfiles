347c462a08ac3b93ad7ea1c549a0fdf16e1552e2
bk2dcradle <ankitsultana@gmail.com>
2016-05-04 18:23:50 +0530

    updates

--- tmux/themes/tmux.solarized-light.sh
+++ tmux/themes/tmux.solarized-light.sh
@@ -56,7 +56,7 @@ set-option -g display-panes-colour $tm_color_inactive
 set-window-option -g clock-mode-colour $tm_color_active
 
 tm_tunes="#[fg=$tm_color_music]#(osascript ~/.utilities/applescripts/itunes.scpt)"
-tm_battery="#(~/.utilities/battery_indicator.sh)"
+tm_battery="#(~/.utilities/sh/battery_indicator.sh)"
 
 tm_date="#[fg=$time_color] %R #[fg=$date_color]%d %b"
 tm_host="#[fg=$tm_color_feature,bold]#h"
@@ -65,7 +65,7 @@ tm_session_name="#[bg=$battery_color]#[fg=$tm_color_feature,bold] $tm_battery"
 status_left=$tm_session_name" #[bg=colour13]#[fg=$battery_color]"$tm_left_separator_black" "
 
 set -g status-left $status_left
-offline='#(~/.utilities/offline.sh) '
+offline='#(~/.utilities/sh/offline.sh) '
 
 mah_separator1="#[fg=$music_color]$tm_right_separator_black#[bg=$music_color]"
 mah_separator2="#[fg=$left_section_color2]$tm_right_separator_black#[bg=$left_section_color2]"
