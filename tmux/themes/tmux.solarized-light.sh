e9c5da15a74e985f981f64b8dbe0538101efa171
Ankit Sultana <ankitsultana@gmail.com>
2023-07-05 06:47:23 +0530

    Update tmux.solarized-light.sh

--- tmux/themes/tmux.solarized-light.sh
+++ tmux/themes/tmux.solarized-light.sh
@@ -28,25 +28,34 @@ set -g status-interval 5
 # set-option -g status-bg colour13
 set-option -g status-fg $tm_color_active
 set-option -g status-bg colour7
-set-option -g status-attr default
+set-option -g status-style default
 
 # default window title colors
-set-window-option -g window-status-fg $tm_color_inactive
-set-window-option -g window-status-bg colour7
-set -g window-status-format "#I #W"
+#set-window-option -g window-status-fg $tm_color_inactive
+#set-window-option -g window-status-bg colour7
+#set -g window-status-format "#I #W"
+
+set-window-option -g window-status-style fg=$tm_color_inactive,bg=colour7
+set -g window-status-format "#I #W#[bg=colour7]#[fg=colour7]$tm_left_separator_black"
 
 # active window title colors
-set-window-option -g window-status-current-fg $tm_color_active
-set-window-option -g window-status-current-bg colour7
-set-window-option -g  window-status-current-format "#[bold]#I #W"
+#set-window-option -g window-status-current-fg $tm_color_active
+#set-window-option -g window-status-current-bg colour7
+#set-window-option -g  window-status-current-format "#[bold]#I #W"
+set-window-option -g window-status-current-style fg=$tm_color_active,bg=colour7
+set -g window-status-current-format "#I #W#[bg=colour7]#[fg=colour7]$tm_left_separator_black"
+set -g window-status-separator ""
 
 # pane border
-set-option -g pane-border-fg $tm_color_inactive
-set-option -g pane-active-border-fg $tm_active_border_color
+#set-option -g pane-border-fg $tm_color_inactive
+#set-option -g pane-active-border-fg $tm_active_border_color
+set-option -g pane-border-style  fg=$tm_color_inactive
+set-option -g pane-active-border-style fg=$tm_active_border_color
 
 # message text
-set-option -g message-bg default
-set-option -g message-fg $tm_color_active
+#set-option -g message-bg default
+#set-option -g message-fg $tm_color_active
+set-option -g message-style bg=default,fg=$tm_color_active
 
 # pane number display
 set-option -g display-panes-active-colour $tm_color_active
